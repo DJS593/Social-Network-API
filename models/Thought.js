a0f5103223b34f332e78e9eb07213307193945e0
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-07-10 09:13:21 -0700

    add models

---
+++ models/Thought.js
@@ -0,0 +1,77 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+
+const ReactionSchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment's _id field
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      max: 280,
+      trim: true
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      // could leave message `required: 'need a username',`
+      min: [1 , 'Thoughts must be between 1 and 280 characters!'],
+      max: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema] 
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+// get total count of comments and replies on retrieval
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.thought.reactions.length;
+  
+  //comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+});
+
+// create the User model using the UserSchema
+const Thought = model('Thought', ThoughtSchema);
+
+// export the User model
+module.exports = User;
---
+++ models/User.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require('mongoose');
+//const { moveMessagePortToContext } = require('worker_threads');
+const moment = require('moment');
+
+
+const UserSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+      required: true,
+      // could leave message `required: 'need a username',`
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/],  // double check
+    },
+    thoughts: [
+      { // not sure what to do here
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      { // not sure what to do for _id
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+// get total count of comments and replies on retrieval
+UserSchema.virtual('friendCount').get(function() {
+  return this.user.friends.length;
+  
+  //comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+});
+
+// create the User model using the UserSchema
+const User = model('User', UserSchema);
+
+// export the User model
+module.exports = User;
